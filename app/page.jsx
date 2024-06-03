04112eb667d8e1e79587a679ed47c0c0d4ccd6b1
sahilaf <sahilfarib320@gmail.com>
2024-06-03 22:57:49 +0600

    links

--- app/page.jsx
+++ app/page.jsx
@@ -8,27 +8,31 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left order-2 xl:order-none"
-          
-          >
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1">
               Hello I'm <br />
               <span className="text-accent">Sahil Al Farib</span>
             </h1>
             <p className="max-e-[500px] mb-9 text-white/80">
-              I exel at crafting elegant digital expriences and I am proficient
+              I excel at crafting elegant digital experiences and I am proficient
               in various programming languages and technologies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
+              <a
+                href="/Sahil_cv.pdf"
+                download
                 className="uppercase flex items-center gap-2 hover:bg-accent hover:text-primary hover:transition-all duration-500"
               >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
               </div>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 import { FaGithub, FaLinkedin, FaTwitter,FaFacebook } from 'react-icons/fa';
 
 const socials =[
-    {icon: <FaGithub/>, path:""},
-    {icon: <FaLinkedin/>, path:""},
-    {icon: <FaTwitter/>, path:""},
-    {icon: <FaFacebook/>, path:""},
+    {icon: <FaGithub/>, path:"https://github.com/sahilaf"},
+    {icon: <FaLinkedin/>, path:"https://www.linkedin.com/in/sahil-al-farib-b45b75270/"},
+    {icon: <FaTwitter/>, path:"https://x.com/SFarib44572"},
+    {icon: <FaFacebook/>, path:"https://www.facebook.com/sahil.alfarib"},
 ];
 const Socials = ({containerStyles,iconStyles}) => {
   return <div className={containerStyles}>
